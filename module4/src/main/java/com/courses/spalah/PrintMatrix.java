38fe620578157e87dcce14c40d59486ca0b91965
Stanislav Pugach <staspugachover900@gmail.com>
2016-07-13 21:48:02 +0300

    Done with Homework 4 modul4

--- module4/src/main/java/com/courses/spalah/PrintMatrix.java
+++ module4/src/main/java/com/courses/spalah/PrintMatrix.java
@@ -48,5 +48,72 @@ public class PrintMatrix {
      * @param args - размер массива
      */
     public static void main(String[] args) {
+        int sizeOfarray = Integer.parseInt(args[0]);
+        String simbol = args[1];
+
+        printSquare(sizeOfarray, simbol);
+        System.out.println();
+        printLeftDownSideOfSquare(sizeOfarray, simbol);
+        System.out.println();
+        printLeftUpSideOfSquare(sizeOfarray, simbol);
+        System.out.println();
+        printRightDownSideOfSquare(sizeOfarray, simbol);
+        System.out.println();
+        printRightUpSideOfSquare(sizeOfarray, simbol);
+    }
+
+    public static void printSquare(int sizeOfarray, String simbol) {
+        for (int i = 0; i < sizeOfarray; i++) {
+            for (int j = 0; j < sizeOfarray; j++) {
+                System.out.print(simbol + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void printLeftDownSideOfSquare(int sizeOfarray, String simbol) {
+        int InSize = 1;
+        for (int i = 0; i < sizeOfarray; i++) {
+            for (int j = 0; j < InSize; j++) {
+                System.out.print(simbol + " ");
+            }
+            System.out.println();
+            InSize++;
+        }
+    }
+
+    public static void printLeftUpSideOfSquare(int sizeOfarray, String simbol) {
+        int InSize = sizeOfarray;
+        for (int i = 0; i < sizeOfarray; i++) {
+            for (int j = 0; j < InSize; j++) {
+                System.out.print(simbol + " ");
+            }
+            System.out.println();
+            InSize--;
+        }
+    }
+
+    public static void printRightDownSideOfSquare(int sizeOfarray, String simbol) {
+        int SpaceCount = sizeOfarray - 1;
+        for (int i = 0; i < sizeOfarray; i++) {
+            for (int j = 0; j < sizeOfarray; j++) {
+                if (SpaceCount > j) System.out.print("  ");
+                else System.out.print(simbol + " ");
+            }
+            System.out.println();
+            SpaceCount--;
+        }
+    }
+
+    public static void printRightUpSideOfSquare(int sizeOfarray, String simbol) {
+        int SpaceCount = 0;
+        for (int i = 0; i < sizeOfarray; i++) {
+            for (int j = 0; j < sizeOfarray; j++) {
+                if (SpaceCount <= j) System.out.print(simbol + " ");
+                else System.out.print("  ");
+            }
+            System.out.println();
+            SpaceCount++;
+        }
     }
 }
