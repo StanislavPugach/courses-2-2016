dd54fc01203197df74b48e958bcd8028ab62c67d
Stanislav Pugach <staspugachover900@gmail.com>
2016-07-13 18:48:01 +0300

    Done with Homework module3

--- module3/src/main/java/com/courses/spalah/Calculator.java
+++ module3/src/main/java/com/courses/spalah/Calculator.java
@@ -10,9 +10,35 @@ import java.io.InputStreamReader;
 public class Calculator {
     public static void main(String[] args) {
         System.out.println("Start calculator");
+        float previous = 0;
+
+        while (true) {
+            String[] arrayOfInputData = readFromConsole();
+            if (arrayOfInputData[0].equals("exit")) break;
+
+            float argument_1 = 0.0F;
+            String operator = "";
+            float argument_2 = 0.0F;
+            if (arrayOfInputData.length == 2) {
+                argument_1 = previous;
+                operator = arrayOfInputData[0];
+                argument_2 = Float.parseFloat(arrayOfInputData[1]);
+            }
+            if (arrayOfInputData.length == 3) {
+                argument_1 = Float.parseFloat(arrayOfInputData[0]);
+                operator = arrayOfInputData[1];
+                argument_2 = Float.parseFloat(arrayOfInputData[2]);
+            }
+
+            float result = culculate(argument_1, argument_2, operator);
+            previous = result;
+            System.out.println(result);
+        }
+
+
     }
 
-    public static float culc(float a, float b, String operator) {
+    public static float culculate(float a, float b, String operator) {
         float result = 0;
         switch (operator) {
             case "+":
