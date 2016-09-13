6b66379fcb444a79a46414bd0d1b7781d30a8511
Stanislav Pugach <staspugachover900@gmail.com>
2016-09-13 18:00:18 +0300

    Done with module 16, but not optimized

---
+++ module16/src/main/java/com/courses/spalah/homework/Main.java
@@ -0,0 +1,10 @@
+package com.courses.spalah.homework;
+
+/**
+ * Created by Stanislav Pugach on 13.09.2016.
+ */
+public class Main {
+    public static void main(String[] args) {
+        Directory directory = new Directory();
+    }
+}
