191b5fb87fc47400f3dc86c0b617ba0a998d2f36
Stanislav Pugach <staspugachover900@gmail.com>
2016-10-03 16:56:33 +0300

    change1

--- module18/src/main/java/spalah/homework/LoadUserServlet.java
+++ module18/src/main/java/spalah/homework/LoadUserServlet.java
@@ -5,12 +5,13 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+
 import com.courses.spalah.homework.Person;
 
 /**
  * Created by Stanislav Pugach on 26.09.2016.
  */
-public class LoadUserServlet extends HttpServlet{
+public class LoadUserServlet extends HttpServlet {
     Person person;
 
     public LoadUserServlet() {
